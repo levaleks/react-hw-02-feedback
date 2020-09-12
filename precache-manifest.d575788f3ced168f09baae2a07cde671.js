052ee673ed3a6dad6d795ec530a8843c8276402f
Oleksii Levchenko <>
2020-09-12 17:14:15 +0300

    Updates

---
+++ precache-manifest.d575788f3ced168f09baae2a07cde671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c60dd8dc97ced827a8d8a4b43943a18",
+    "url": "/react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "1e1adceb5d144fda48b0",
+    "url": "/react-hw-02-feedback/static/css/main.5fa49014.chunk.css"
+  },
+  {
+    "revision": "c5bfb431812cfec7b726",
+    "url": "/react-hw-02-feedback/static/js/2.ad9814fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hw-02-feedback/static/js/2.ad9814fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1adceb5d144fda48b0",
+    "url": "/react-hw-02-feedback/static/js/main.7a0ed973.chunk.js"
+  },
+  {
+    "revision": "b8162755fe890df6eb81",
+    "url": "/react-hw-02-feedback/static/js/runtime-main.f7c9947b.js"
+  }
+]);
